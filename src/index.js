dcd939f65bf8d262b96c3b793800241ea38c2aed
shambu <shambuqwerty@gmail.com>
2021-12-04 23:58:42 +0530

    v1.0

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
+		<Router basename="/manga-viewer">
 			<App />
 		</Router>
 	</React.StrictMode>,
