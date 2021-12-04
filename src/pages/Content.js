7d8c77e6a73b3687a3cb02b7d34a2e8cb072e8d9
shambu <shambuqwerty@gmail.com>
2021-12-05 00:03:59 +0530

    v1.0

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from "react";
 import { metadata_url } from "../utils";
 import { Link } from "react-router-dom";
 
