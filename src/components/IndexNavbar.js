0ce1db4bfdb4b114e8d87fa979c2e4682621bf6b
shambu <shambuqwerty@gmail.com>
2022-01-26 08:56:48 +0530

    added spinner

--- src/components/IndexNavbar.js
+++ src/components/IndexNavbar.js
@@ -44,14 +44,19 @@ function IndexNavbar({ isLoading, index }) {
 					role="tabpanel"
 					aria-labelledby="index-tab"
 				>
-					{!isLoading &&
+					{isLoading ? (
+						<div class="spinner-border text-primary" role="status">
+							<span class="sr-only">Loading...</span>
+						</div>
+					) : (
 						Object.entries(index).map(([key, value]) => {
 							return (
 								<div key={key} className="index-element">
 									<Link to={`/manga/${key}`}>{value}</Link>
 								</div>
 							);
-						})}
+						})
+					)}
 				</div>
 				<div
 					className="tab-pane fade pl-5 pt-3 pb-2 pr-5"
